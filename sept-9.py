d7a65053ec433862a9b6dcc5a43ddfba7a8d7618
Govind9066 <gadekargovind575@gmail.com>
2025-09-09 23:13:32 +0530

    check prime number

--- sept-9.py
+++ sept-9.py
@@ -1,1 +1,16 @@
 print("hello")
+
+print("Prime Numbers : ")
+
+num=4
+check=0
+
+for i in range(2,num):
+    if num % i ==0:
+        check+=1
+        break
+
+if check == 0:
+    print("Prime Number")
+else:
+    print("Not Prime Number")
