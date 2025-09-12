66498fac486c4e6dea267ccde5a2f6dccb58a744
Govind9066 <gadekargovind575@gmail.com>
2025-09-12 21:29:31 +0530

    Rev String

--- sept-12.py
+++ sept-12.py
@@ -8,3 +8,11 @@ if year % 400 ==0 or year % 4==0 and year % 100 != 0:
     print("Yes it is leap year")
 else:
     print("NO it is not leap year")
+
+
+print("Rev String")
+Str="Govind"
+
+print(Str[::-1])
+
+
