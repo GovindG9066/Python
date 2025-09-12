cf41919b60713d5764b9b2fbad0e6bae31e50811
Govind9066 <gadekargovind575@gmail.com>
2025-09-12 21:24:56 +0530

    Leap Year or Not

--- sept-12.py
+++ sept-12.py
@@ -1,1 +1,10 @@
 print("Sept-12")
+
+print("Leap Year : ")
+
+year=2016
+
+if year % 400 ==0 or year % 4==0 and year % 100 != 0:
+    print("Yes it is leap year")
+else:
+    print("NO it is not leap year")
