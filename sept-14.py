cafb2b6457350c98d2dde2fd9e8230710d7d9556
Govind9066 <gadekargovind575@gmail.com>
2025-09-14 21:46:19 +0530

    armstrong number

---
+++ sept-14.py
@@ -0,0 +1,17 @@
+print("sept-14")
+
+print("Armstrong Number : ")
+
+num=int(input("Enter the number : "))
+original=num
+arm=0
+
+while num != 0:
+    digit=num % 10
+    arm=arm+pow(digit,3)
+    num//=10
+
+if original== arm:
+    print(original," is an armstrong number")
+else:
+    print(original," is not an armstrong number")
