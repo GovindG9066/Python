c5433e1b713c94a5a74db1c5a66f7c637dbf4d4f
Govind9066 <gadekargovind575@gmail.com>
2025-09-14 21:48:54 +0530

    Prime Number

--- sept-14.py
+++ sept-14.py
@@ -32,3 +32,18 @@ if rev==org:
     print("palandrome number")
 else:
     print("Not palandrome")
+
+print("Prime Numbers : ")
+
+num=4
+check=0
+
+for i in range(2,num):
+    if num % i ==0:
+        check+=1
+        break
+
+if check == 0:
+    print("Prime Number")
+else:
+    print("Not Prime Number")
