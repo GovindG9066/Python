3c4bb0c0d9721dc613998597815d416dcab3dba5
Govind9066 <gadekargovind575@gmail.com>
2025-09-14 21:47:35 +0530

    Add palindrome number check to sept-14.py

--- sept-14.py
+++ sept-14.py
@@ -15,3 +15,20 @@ if original== arm:
     print(original," is an armstrong number")
 else:
     print(original," is not an armstrong number")
+
+
+print("Palandrome number : ")
+
+num=1223221
+org=num
+rev=0
+
+while num != 0:
+    digit=num % 10
+    rev=rev*10+digit
+    num//=10
+
+if rev==org:
+    print("palandrome number")
+else:
+    print("Not palandrome")
