af129d054b56c2a61ae60249cfebda644bd52877
Govind9066 <gadekargovind575@gmail.com>
2025-09-15 12:13:01 +0530

    factorial using recursion

--- sept-15.py
+++ sept-15.py
@@ -34,3 +34,11 @@ op=1
 for i in range(num,0,-1):
     op=op*i
 print(op)
+
+print("Factorial using recursion : ")
+def factorial(n):
+    if n==0:
+        return 1
+    else:
+        return n*factorial(n-1)
+print(factorial(5)) 
