fe3f15317fcd7341cfb8ffa903acfb16001db940
Govind9066 <gadekargovind575@gmail.com>
2025-09-23 23:19:21 +0530

    in build function in the dictionary

--- sept-23.py
+++ sept-23.py
@@ -4,3 +4,7 @@ print("Dictionary : ")
 
 dict={"Id":1,"name":"Govind","RollNo":18}
 print(dict)
+
+print(dict.keys()) # print all keyes
+print(dict.values()) # print all values
+print(dict.items()) # print items 
