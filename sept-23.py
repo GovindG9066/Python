010cfbd5712b69f093bfb6492835a951a8c65e12
Govind9066 <gadekargovind575@gmail.com>
2025-09-23 23:23:02 +0530

    update delete and add new key value pair in dictionary

--- sept-23.py
+++ sept-23.py
@@ -8,3 +8,12 @@ print(dict)
 print(dict.keys()) # print all keyes
 print(dict.values()) # print all values
 print(dict.items()) # print items 
+
+dict["Id"]=2
+print(dict) # update id
+
+dict["City"]="Pune"
+print(dict)  # add a new key value pair
+
+dict.pop("City")
+print(dict)
