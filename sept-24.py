92ac33205447c2d69b9ee311109922fab796f8f3
Govind9066 <gadekargovind575@gmail.com>
2025-09-24 22:15:20 +0530

    some programs

--- sept-24.py
+++ sept-24.py
@@ -1,1 +1,34 @@
 print("sept-24")
+
+print("Armstrong Number : ")
+
+num=int(input("Enter the number : "))
+original=num
+arm=0
+
+while num != 0:
+    digit=num % 10
+    arm=arm+pow(digit,3)
+    num//=10
+
+if original== arm:
+    print(original," is an armstrong number")
+else:
+    print(original," is not an armstrong number")
+
+
+print("Palandrome number : ")
+
+num=1223221
+org=num
+rev=0
+
+while num != 0:
+    digit=num % 10
+    rev=rev*10+digit
+    num//=10
+
+if rev==org:
+    print("palandrome number")
+else:
+    print("Not palandrome")
