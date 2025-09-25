388bf7de31048fd98b0bdb964a5cc4327135c201
Govind9066 <gadekargovind575@gmail.com>
2025-09-25 16:02:12 +0530

    operations on sets

--- sept-25.py
+++ sept-25.py
@@ -15,3 +15,19 @@ print(fruits)
 
 fruits.remove("apple")
 print(fruits)
+
+print("Set operations :")
+num1={7,8,8,4,6,8,5,5,5}
+num2={6,7,6,4,8,2,16,41}
+
+print(num1)
+print(num2)
+
+print(num1 | num2) # print all the items in both sets also remove duplicate
+
+print(num1 & num2) # print the intersection in (same in both sets)
+
+print(num1 - num2) # items which in num 1 but not in num 2
+print(num2 - num1) # items which in num 2 but not in num 1 (differencieate)
+
+print(num1 ^ num2) # items which are difference in both 
