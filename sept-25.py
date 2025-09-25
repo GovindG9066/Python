7e8281c728c08b4e05f49eaddd645eec86f682f5
Govind9066 <gadekargovind575@gmail.com>
2025-09-25 15:53:31 +0530

    add remove in set

--- sept-25.py
+++ sept-25.py
@@ -4,3 +4,14 @@ print("learning sets in python : ")
 
 my_set={4,8,6,8,64,954,4}
 print(my_set)
+
+print("add new items in set : ")
+
+fruits={"mango","orange","apple","banana"}
+print(fruits)
+print(type(fruits))
+fruits.add("cherry")
+print(fruits)
+
+fruits.remove("apple")
+print(fruits)
