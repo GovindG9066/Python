557b4c285a8859b32257c458f2690f70e6d919cb
Govind9066 <gadekargovind575@gmail.com>
2025-10-06 22:44:28 +0530

    Calculator using switch cases in python

--- sept-6.py
+++ sept-6.py
@@ -1,1 +1,31 @@
 print("sept-6")
+first=int(input("Enter the first number :"))
+second=int(input("Enter the second number : "))
+print("Operator + - * / %")
+oper=input("Enter the Operator : ")
+print("calculator using switch :")
+
+def calculator(a,b,oper):
+    match oper:
+        case "+":
+            print("addition is  : ")
+            return a+b
+        case "-":
+            print("Subtraction is : ")
+            return a-b
+        case "*":
+            print("Multiplication is : ")
+            return a*b
+        case "/":
+            if b==0:
+                print("0 not allowed to divide")
+            else:
+                print("Division is :")
+                return a/b
+        case "%":
+            print("Reminder is : ")
+            return a%b
+        
+
+answer=calculator(first,second,oper)
+print(answer) 
