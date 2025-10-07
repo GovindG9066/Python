b83251fbe729247862029b10483193e658b95e16
Govind9066 <gadekargovind575@gmail.com>
2025-10-07 21:39:22 +0530

    learning python class instance variables and more things

--- Oct-7.py
+++ Oct-7.py
@@ -1,1 +1,17 @@
 print("Oct-7")
+
+print("Instance variable :")
+
+class Mywork:
+    def disp(self):
+        print("I am the instance method")
+    
+def main():
+    print("Inside the main method")
+    obj=Mywork()
+    obj.disp()
+    print("End of main")
+
+if __name__=="__main__":
+    main()
+    print("End of application")
