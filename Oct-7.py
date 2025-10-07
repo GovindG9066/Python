47243ad1ce291cf9bf174608c89f0fafb0b31d19
Govind9066 <gadekargovind575@gmail.com>
2025-10-07 21:44:48 +0530

    constructor or destroctor program try

--- Oct-7.py
+++ Oct-7.py
@@ -15,3 +15,18 @@ def main():
 if __name__=="__main__":
     main()
     print("End of application")
+
+
+class demo:
+    def __init__(self):
+        print("I am the constructor...")
+    def __del__(self):
+        print("Inside distructor...")
+
+def main():
+    print("Inside main function...")
+    obj=demo()
+    print("End of the main method...")
+if __name__=="__main__":
+    main()
+    print("End of the program")
