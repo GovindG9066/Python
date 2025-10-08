35ebc400d23e93ffc924f226074cb9cb1372a5d4
Govind9066 <gadekargovind575@gmail.com>
2025-10-08 21:35:18 +0530

    Linked list print

--- Oct-8.py
+++ Oct-8.py
@@ -1,1 +1,27 @@
 print("Oct-8")
+
+print("Linked list traversal...")
+
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+n1=Node(56)
+n2=Node(5)
+n3=Node(556)
+n4=Node(65)
+
+n1.next=n2
+n2.next=n3
+n3.next=n4
+
+head=n1
+
+def traversal(head):
+    current=head
+    while current:
+        print(current.data,end="->")
+        current=current.next
+    print("Null")
+
+traversal(head)
