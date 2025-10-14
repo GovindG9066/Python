5761abb287662cc7ebe43a9befc1b9b15bb693ea
Govind9066 <gadekargovind575@gmail.com>
2025-10-14 16:18:24 +0530

    even number

--- Oct-14.py
+++ Oct-14.py
@@ -13,7 +13,14 @@ for i in range(count+1):
     print(i * "* ")
 
 print(" ")
+count=5
+for i in range(count+1):
+    print("* " * (count-i))
 
 count=5
 for i in range(count+1):
     print("  " * (count-i) + "* " * i + "* " * (i-1))
+
+count=20
+for i in range(2,count+1,2):
+    print(i)
