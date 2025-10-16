4d8564e7677b3037658368a44b438cdb73cad5ac
Govind9066 <gadekargovind575@gmail.com>
2025-10-16 21:58:01 +0530

    Up side down pyramid trangle pattarn

--- Oct-16.py
+++ Oct-16.py
@@ -4,3 +4,10 @@ print("Pyramid triangle : ")
 count=5
 for i in range(1,count+1):
     print("  " * (count-i) + "* " * i + "* " * (i-1))
+
+print("Up side down Pyramid Triangle :")
+
+count = 5  # define number of rows
+
+for i in range(count, 0, -1):
+    print("  " * (count - i) + "* " * (2 * i - 1))
