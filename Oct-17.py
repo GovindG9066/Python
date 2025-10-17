21b07002fff549bb43f6032b6cda5af8acc84597
Govind9066 <gadekargovind575@gmail.com>
2025-10-17 22:23:35 +0530

    Star Pattern up side down flip right angle triangle

--- Oct-17.py
+++ Oct-17.py
@@ -4,3 +4,8 @@ print("Star pattern : ")
 count=5
 for i in range(count+1):
     print("  " * (count-i) + "* " * i)
+
+print("Star Pattern up side down flip right angle triangle : ")
+
+for i in range(count+1):
+    print("  " * i +"* " * (count-i))
