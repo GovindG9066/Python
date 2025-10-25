545fd4ee8a1f475cebf9db30814eaaaeaf16367e
Govind9066 <gadekargovind575@gmail.com>
2025-10-25 22:14:31 +0530

    X pattern

--- Oct-25.py
+++ Oct-25.py
@@ -31,3 +31,13 @@ print("")
 
 for i in range(count+1):
     print(" " * i + "* " * (count-i))
+
+
+print("X pattern using stars :")
+
+count=5
+
+for i in range(count):
+    print(("  " * i + "* " + "  " * (count-i-1)) + ("  " * (count-i-1) + "* "   ))
+for i in range(count):
+    print(("  " * (count-i-1) + "* " + "  " * i)+("  " * i + "* "))
