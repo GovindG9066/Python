b393dbd048cc14a0294c28480febdfc01cda8698
Govind9066 <gadekargovind575@gmail.com>
2025-10-27 23:16:17 +0530

    Star Patterns

--- Oct-27.py
+++ Oct-27.py
@@ -1,1 +1,33 @@
 print("Oct-27")
+
+print("Star Pattern")
+
+count=5
+
+for i in range(1,count+1):
+    print("* " * i)
+
+print("")
+
+for i in range(1,count+1):
+    print("* " * (count+1-i))
+
+print("")
+
+for i in range(count+1):
+    print("  " * i + "* " * (count-i))
+
+print("")
+
+for i in range(count+1):
+    print("  " * (count-i) + "* " * i)
+
+print("")
+
+for i in range(count+1):
+    print(" " * (count-i) + "* " * i )
+
+print("")
+
+for i in range(count+1):
+    print(" " * i + "* " * (count-i))
