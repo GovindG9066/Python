a46c306bbf5dc6d7df48e69a03f028e25f48a80f
Govind9066 <gadekargovind575@gmail.com>
2025-10-29 22:27:53 +0530

    Armstrong Number

--- Oct-29.py
+++ Oct-29.py
@@ -1,1 +1,16 @@
 print("Oct-29")
+
+print("Armstrong Number :")
+
+num=153
+original=num
+arm=0
+while num:
+    digit=num % 10
+    arm+=pow(digit,3)
+    num=num//10
+
+if original==arm:
+    print("Armstrong")
+else:
+    print("Not armstrong")
