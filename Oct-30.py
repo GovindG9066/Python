2ba35ae01f1060a812ef98192113d0e57f052b5b
Govind9066 <gadekargovind575@gmail.com>
2025-10-30 22:36:06 +0530

    Linked List

--- Oct-30.py
+++ Oct-30.py
@@ -1,1 +1,28 @@
 print("Oct-30")
+
+
+print("Linked list...")
+
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+
+n1=Node(545)
+n2=Node(54)
+n3=Node(55)
+n4=Node(45)
+
+n1.next=n2
+n2.next=n3
+n3.next=n4
+
+head=n1
+
+current=head
+
+while current:
+    print(current.data,end="->")
+    current=current.next
+print("Null")
+
