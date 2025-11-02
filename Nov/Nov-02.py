d21f5e3855ef3e6fa31525e61a9895d9322878e1
Govind9066 <gadekargovind575@gmail.com>
2025-11-02 18:03:54 +0530

    solve question 1

--- Nov/Nov-02.py
+++ Nov/Nov-02.py
@@ -1,1 +1,27 @@
 print("Nov-02")
+
+print("Python Paper Pattern 2024")
+
+print("1st question")
+
+dict={
+    "id":1, 
+    "name":"Govind",
+    "Address":"Pune",
+    "Roll No":18
+    }
+
+print(dict.keys())
+
+dict.update({"Age":21})
+
+print(dict)
+
+dict.pop("Address")
+
+print(dict)
+
+dict["name"]="Gondu"
+
+print(dict)
+
