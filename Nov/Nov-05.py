bd445fb9e3471f1cbb52456374b8fc516f1fc74f
Govind9066 <gadekargovind575@gmail.com>
2025-11-05 22:17:32 +0530

    Star Patterns

--- Nov/Nov-05.py
+++ Nov/Nov-05.py
@@ -1,1 +1,16 @@
 print("Nov-05")
+
+print("Star pattern : ")
+count=5
+for i in range(count+1):
+    print("  " * (count-i) + "* " * i)
+
+print("Star Pattern up side down flip right angle triangle : ")
+
+for i in range(count+1):
+    print("  " * i +"* " * (count-i))
+
+count = 5
+for i in range(1, count+1):
+    # Spaces before the first star
+    print("  " * (count-i) + "* " + "  " * (i-1))
