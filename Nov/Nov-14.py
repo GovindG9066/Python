4183b19c30d8d0a98e41b1bb82d20907b335fcb2
Govind9066 <gadekargovind575@gmail.com>
2025-11-14 22:43:54 +0530

    BInary

--- Nov/Nov-14.py
+++ Nov/Nov-14.py
@@ -1,1 +1,48 @@
 print("Nov-14")
+
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.left=None
+        self.right=None
+
+class Binary:
+    def __init__(self):
+        self.root=None
+
+    def insert(self,new_data):
+        new_node=Node(new_data)
+        if self.root is None:
+            self.root=new_node
+
+        curr=self.root
+        while True:
+            if new_data < curr.data:
+                if curr.left is None:
+                    curr.left=new_node
+                    break
+                else:
+                    curr=curr.left
+            elif new_data > curr.data:
+                if curr.right is None:
+                    curr.right=new_node
+                    break
+                else:
+                    curr=curr.right
+            else:
+                break
+    def inorder(self, node):
+        if node is not None:
+            self.inorder(node.left)
+            print(node.data, end=" ")
+            self.inorder(node.right)
+
+if __name__=="__main__":
+    bt=Binary()
+
+    bt.insert(81)
+    bt.insert(32)
+    bt.insert(684)
+    bt.insert(65)
+
+    bt.inorder(bt.root)
