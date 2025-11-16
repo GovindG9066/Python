53061bd725f4103a18ff3e18003d4fb07f869c21
Govind9066 <gadekargovind575@gmail.com>
2025-11-16 22:24:04 +0530

    Armstron Number

--- Nov/Nov-16.py
+++ Nov/Nov-16.py
@@ -1,1 +1,16 @@
 print("Nov-16")
+
+print("Armstrong Number :")
+
+num=153
+original=num
+arm=0
+while num:
+    digit=num%10
+    arm+=pow(digit,3)
+    num=num//10
+
+if original==arm:
+    print("Arm")
+else:
+    print("Not Arm")
