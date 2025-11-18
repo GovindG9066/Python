8e1d25f510762ebdb5836b94aa22ded8a3d198f6
Govind9066 <gadekargovind575@gmail.com>
2025-11-18 09:00:00 +0530

    Backdated: commit for 18 Nov 2025

---
+++ Nov/Nov-18.py
@@ -0,0 +1,17 @@
+print("Nov-18")
+
+def binary(arr,l,r,x):
+    if l<=r:
+        mid=(l+r)//2
+        if arr[mid] == x:
+            return mid
+        elif arr[mid] > x :
+            return binary(arr,l,mid-1,x)
+        else:
+            return binary(arr,mid+1,r,x)
+    return -1
+
+arr=[1,3,5,7,8,9]
+x=8
+
+print(f"Element is present at {binary(arr,0,len(arr)-1,x)}")
