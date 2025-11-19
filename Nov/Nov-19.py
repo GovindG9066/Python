70f8bda6f1a4c26db939bb36ae050254778f6ee2
Govind9066 <gadekargovind575@gmail.com>
2025-11-19 21:52:58 +0530

    Not fount

--- Nov/Nov-19.py
+++ Nov/Nov-19.py
@@ -1,6 +1,9 @@
 import Nov_18
 
 arr1=[2,4,6,8,10,12]
+arr2=[12,85,99,654,949]
 x=6
 output=Nov_18.binary(arr1,0,len(arr1)-1,x)
+output1=Nov_18.binary(arr2,0,len(arr2)-1,x)
 print(output)
+print(output1)
