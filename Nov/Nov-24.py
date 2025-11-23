ac25a6854210c21e08be38df67068e5a969a549c
Govind9066 <gadekargovind575@gmail.com>
2025-11-24 00:44:17 +0530

    use of the finally key word

---
+++ Nov/Nov-24.py
@@ -0,0 +1,20 @@
+print("Nov-24")
+
+print("finally key word:")
+
+a="Govind"
+try:
+    print("My name is "+a) 
+except:
+    print("Some thing went wrong")
+finally:
+    print("This is the end...")
+
+print("\n")
+
+try:
+    print("My name is "+b) 
+except:
+    print("Some thing went wrong")
+finally:
+    print("This is the end...")
