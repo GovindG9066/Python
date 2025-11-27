0df7319a448b7dcf43cd10af3a6eae0c7576b933
Govind9066 <gadekargovind575@gmail.com>
2025-11-27 13:11:26 +0530

    List program

--- Nov/Nov-27-PythonPractice.py
+++ Nov/Nov-27-PythonPractice.py
@@ -1,86 +1,103 @@
-print("1st mearge 2 dict")
+# print("1st mearge 2 dict")
 
-stu_details={"id":1,"name":"Govind","age":21,}
-stu_add={"At/Post":"Yenere","Tel":"Junnar","Dist":"Pune","Pin-Code":410502}
+# stu_details={"id":1,"name":"Govind","age":21,}
+# stu_add={"At/Post":"Yenere","Tel":"Junnar","Dist":"Pune","Pin-Code":410502}
 
-print(stu_details)
-print(stu_add)
-
-
-print("2nd num div by 5")
-
-tup=(12,96,865,35,66,26,65,95,358,26,35)
-
-for i in tup:
-    if i % 5 == 0:
-        print(i)
+# print(stu_details)
+# print(stu_add)
 
 
-print("3rd Handle custom exception handling")
+# print("2nd num div by 5")
+
+# tup=(12,96,865,35,66,26,65,95,358,26,35)
+
+# for i in tup:
+#     if i % 5 == 0:
+#         print(i)
 
 
-try:
-    age=18
-    if age
-
-print("4th even num in list")
-
-list1=[15,7,45,47,84,45,4,61,34,12]
-for num in list1:
-    if num % 2 == 0:
-        print(num)
-
-print("5th perfect no between 1 to n")
-
-6==divide(1,2,3)==1+2+3=6
-
-n=50
-
-for num in range(1,n):
-    original=num
-    x=0
-    for i in range(1,num):
-        if num % i ==0:
-            x+=i
-    if original==x:
-        print(original)
+# print("3rd Handle custom exception handling")
 
 
-print("6th largest num from given list ")
+# try:
+#     age=18
+#     if age
 
-list2=[-1,-8,-6,-89,-5,-9,-2]
+# print("4th even num in list")
 
-number=0
-for i in list2:
-    if i>number:
-        number=i
-print(number)
+# list1=[15,7,45,47,84,45,4,61,34,12]
+# for num in list1:
+#     if num % 2 == 0:
+#         print(num)
 
-print("7th even num from 1 to 50")
+# print("5th perfect no between 1 to n")
 
-n=50
-list3=[]
+# 6==divide(1,2,3)==1+2+3=6
 
-for i in range(n+1):
-    if i % 2 == 0:
-        list3.append(i)
-print(list3)
+# n=50
 
-print("8th Palandrom num :")
+# for num in range(1,n):
+#     original=num
+#     x=0
+#     for i in range(1,num):
+#         if num % i ==0:
+#             x+=i
+#     if original==x:
+#         print(original)
 
-num=12321
-original=num
-rev=0
 
-while num:
-    digit=num % 10
-    rev=10*rev+digit
-    num=num//10
+# print("6th largest num from given list ")
 
-if original == rev:
-    print(f"{original} is Palandrome num")
-else:
-    print(f"{original} is Not Palandrome num")
+# list2=[-1,-8,-6,-89,-5,-9,-2]
+
+# number=0
+# for i in list2:
+#     if i>number:
+#         number=i
+# print(number)
+
+# print("7th even num from 1 to 50")
+
+# n=50
+# list3=[]
+
+# for i in range(n+1):
+#     if i % 2 == 0:
+#         list3.append(i)
+# print(list3)
+
+# print("8th Palandrom num :")
+
+# num=12321
+# original=num
+# rev=0
+
+# while num:
+#     digit=num % 10
+#     rev=10*rev+digit
+#     num=num//10
+
+# if original == rev:
+#     print(f"{original} is Palandrome num")
+# else:
+#     print(f"{original} is Not Palandrome num")
+
+
+
+l1=[1,2,3,4,5]
+l2=[11,12,13,14,15]
+new_list=[]
+
+for i in l1:
+    if i % 2 != 0:
+        new_list.append(i)
+    
+    
+for j in l2:
+    if j % 2 == 0:
+        new_list.append(j)
+
+print(new_list)
 
 
 
