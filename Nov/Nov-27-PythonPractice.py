c131c695f41cd031a6a746c8adb732ff1c8ac542
Govind9066 <gadekargovind575@gmail.com>
2025-11-27 12:51:36 +0530

    Some programs form clg  to solve  practically

---
+++ Nov/Nov-27-PythonPractice.py
@@ -0,0 +1,90 @@
+print("1st mearge 2 dict")
+
+stu_details={"id":1,"name":"Govind","age":21,}
+stu_add={"At/Post":"Yenere","Tel":"Junnar","Dist":"Pune","Pin-Code":410502}
+
+print(stu_details)
+print(stu_add)
+
+
+print("2nd num div by 5")
+
+tup=(12,96,865,35,66,26,65,95,358,26,35)
+
+for i in tup:
+    if i % 5 == 0:
+        print(i)
+
+
+print("3rd Handle custom exception handling")
+
+
+try:
+    age=18
+    if age
+
+print("4th even num in list")
+
+list1=[15,7,45,47,84,45,4,61,34,12]
+for num in list1:
+    if num % 2 == 0:
+        print(num)
+
+print("5th perfect no between 1 to n")
+
+6==divide(1,2,3)==1+2+3=6
+
+n=50
+
+for num in range(1,n):
+    original=num
+    x=0
+    for i in range(1,num):
+        if num % i ==0:
+            x+=i
+    if original==x:
+        print(original)
+
+
+print("6th largest num from given list ")
+
+list2=[-1,-8,-6,-89,-5,-9,-2]
+
+number=0
+for i in list2:
+    if i>number:
+        number=i
+print(number)
+
+print("7th even num from 1 to 50")
+
+n=50
+list3=[]
+
+for i in range(n+1):
+    if i % 2 == 0:
+        list3.append(i)
+print(list3)
+
+print("8th Palandrom num :")
+
+num=12321
+original=num
+rev=0
+
+while num:
+    digit=num % 10
+    rev=10*rev+digit
+    num=num//10
+
+if original == rev:
+    print(f"{original} is Palandrome num")
+else:
+    print(f"{original} is Not Palandrome num")
+
+
+
+
+
+    
+ 
