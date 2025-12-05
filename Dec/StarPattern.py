c7b8944be59f12413d1209c53f2018bac1b18b24
Govind9066 <gadekargovind575@gmail.com>
2025-12-05 16:10:22 +0530

    flip up side down right angle triangle

--- Dec/StarPattern.py
+++ Dec/StarPattern.py
@@ -6,12 +6,17 @@ for i in range(1,count+1):
     print("* "*i)
 
 print("Up Side Down Simple Star Pattern :")
-
-for i in range(1,count+1):
-    print("* " * (count+1-i))
+c=count+1
+for i in range(1,c):
+    print("* " * (c-i))
 
 
 print("Simple flip right angle triangle : ")
 
 for i in range(1,count+1):
     print("  "*(count-i)+"* " *i)
+
+print("Up Side Down flip Right angle trangle :")
+
+for i in range(1,count+1):
+    print("  " * i + "* " * (count+1-i))
