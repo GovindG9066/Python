ca37efe834375a6534c6e687cb34034727f3650b
Govind9066 <gadekargovind575@gmail.com>
2025-12-05 15:43:37 +0530

    Valid URL checking

--- Dec/regularExpression.py
+++ Dec/regularExpression.py
@@ -7,3 +7,14 @@ if re.match(pattern,email):
     print("Valid Email")
 else:
     print("Invalid Email")
+
+
+print("Next...")
+
+url="https://www.google.com"
+pattarnURL=r"^https?://[A-Za-z0-9.-]+\.[A-Za-z]{2,}$"
+
+if re.match(pattarnURL,url):
+    print("Valid URL",url)
+else:
+    print("Invalid URL")
